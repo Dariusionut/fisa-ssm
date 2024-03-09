8957b6e31f1eb3de1936529dd63ecc1a10b520c0
Darius <dariustinculescu@gmail.com>
2024-03-09 19:21:15 +0200

    init project

---
+++ src/test/java/ro/fisa/ssm/FisaSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fisa.ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisaSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
